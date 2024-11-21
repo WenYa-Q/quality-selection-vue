ab497def2c8fc161896eb911e89015f913000c37
wenya <[wenya.com]>
2024-11-21 21:01:57 +0800

    feat: :sparkles: 用户页面添加用户功能

--- src/api/sysUser.js
+++ src/api/sysUser.js
@@ -1,10 +1,19 @@
-import request from '@/utils/request'
+import request from "@/utils/request";
 
 // 分页查询
-export const GetSysUserListByPage = (pageNum , pageSize , queryDto) => {
+export const GetSysUserListByPage = (pageNum, pageSize, queryDto) => {
+  return request({
+    url: "/system/sysUser/findByPage/" + pageNum + "/" + pageSize,
+    method: "get",
+    params: queryDto,
+  });
+};
+
+// 新增用户的方法
+export const SaveSysUser = (data) => {
     return request({
-        url: "/system/sysUser/findByPage/" + pageNum + "/" + pageSize,
-        method: 'get',
-        params: queryDto
+        url: "/system/sysUser/saveSysUser",
+        method: "post",
+        data
     })
 }
