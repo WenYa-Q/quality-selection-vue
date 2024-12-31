c86e2adbe220e129f942df2a75b4987b3885480c
wenya <1296426842@qq.com>
2024-12-31 11:55:16 +0800

    feat: :sparkles: 品牌管理页面

---
+++ src/api/brand.js
@@ -0,0 +1,11 @@
+import request from "@/utils/request";
+
+const api_name = "/product/brand";
+
+// 分页列表
+export const GetBrandPageList = (page, limit) => {
+  return request({
+    url: `${api_name}/${page}/${limit}`,
+    method: "get",
+  });
+};
