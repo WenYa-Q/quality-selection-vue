d9d82d9aa8f04024e30804aa43ffb3f35f7b7788
wenya <1296426842@qq.com>
2024-12-31 10:56:19 +0800

    feat: :sparkles: 分类管理页面增加导出、导入功能

--- src/views/product/category.vue
+++ src/views/product/category.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="tools-div">
-    <el-button type="success" size="small">导出</el-button>
-    <el-button type="primary" size="small">导入</el-button>
+    <el-button type="success" size="small" @click="exportData">导出</el-button>
+    <el-button type="primary" size="small" @click="importData">导入</el-button>
   </div>
 
   <!---懒加载的树形表格-->
@@ -24,11 +24,28 @@
     </el-table-column>
     <el-table-column prop="createTime" label="创建时间" />
   </el-table>
+
+  <el-dialog v-model="dialogImportVisible" title="导入" width="30%">
+    <el-form label-width="120px">
+      <el-form-item label="分类文件">
+        <el-upload
+          class="upload-demo"
+          action="http://localhost:8080/product/category/importData"
+          :on-success="onUploadSuccess"
+          :headers="headers"
+        >
+          <el-button type="primary">上传</el-button>
+        </el-upload>
+      </el-form-item>
+    </el-form>
+  </el-dialog>
 </template>
 
 <script setup>
 import { onMounted, ref } from "vue";
-import { FindCategoryByParentId } from "@/api/category";
+import { FindCategoryByParentId, ExportCategoryData } from "@/api/category";
+import { useApp } from "@/pinia/modules/app";
+import { ElMessage } from "element-plus";
 
 // 定义list属性模型
 const list = ref([]);
@@ -46,21 +63,56 @@ const fetchData = async (row, treeNode, resolve) => {
   // 返回数据
   resolve(data);
 };
+
+/**
+ * 导出表格
+ */
+function exportData() {
+  ExportCategoryData().then((res) => {
+    const blob = new Blob([res]);
+
+    const link = document.createElement("a");
+    link.href = window.URL.createObjectURL(blob);
+    link.download = "分类数据.xlsx";
+
+    link.click();
+  });
+}
+
+// 文件上传相关变量以及方法定义
+const dialogImportVisible = ref(false);
+const headers = {
+  // 从pinia中获取token，在进行文件上传的时候将token设置到请求头中
+  token: useApp().authorization.token,
+};
+
+function importData() {
+  dialogImportVisible.value = true;
+}
+
+// 上传文件成功以后要执行方法
+const onUploadSuccess = async (response, file) => {
+  ElMessage.success("操作成功");
+  dialogImportVisible.value = false;
+  const { data } = await FindCategoryByParentId(0);
+  list.value = data;
+};
 </script>
 
 <style scoped>
 .search-div {
+  padding: 10px;
   margin-bottom: 10px;
-  padding: 10px;
+  background-color: #fff;
   border: 1px solid #ebeef5;
   border-radius: 3px;
-  background-color: #fff;
 }
+
 .tools-div {
-  margin: 10px 0;
   padding: 10px;
+  margin: 10px 0;
+  background-color: #fff;
   border: 1px solid #ebeef5;
   border-radius: 3px;
-  background-color: #fff;
 }
 </style>
